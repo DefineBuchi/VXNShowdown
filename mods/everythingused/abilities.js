39494ae483fe8a390810e0f11278b70aaa66e6b9
VXN <ChoiceBandLunala@users.noreply.github.com>
2017-06-06 11:46:26 -0400

    Slush rush is immune to hail

--- mods/everythingused/abilities.js
+++ mods/everythingused/abilities.js
@@ -1,5 +1,11 @@
 'use strict';
 
 exports.BattleAbilities = {
-
+	"slushrush": {
+		inherit: true,
+		onImmunity: function (type, pokemon) {
+			if (type === 'hail') return false;
+		},
+		
+	},
 };
