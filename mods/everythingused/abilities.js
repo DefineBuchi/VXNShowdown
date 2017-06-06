be5942b8c7ff5ab15ea5aa20e6b16ddbf343ec0f
VXN <ChoiceBandLunala@users.noreply.github.com>
2017-06-06 13:13:56 -0400

    Update abilities.js

--- mods/everythingused/abilities.js
+++ mods/everythingused/abilities.js
@@ -8,4 +8,21 @@ exports.BattleAbilities = {
 		},
 		
 	},
+	"liquidvoice": {
+		inherit: true,
+		onModifyMovePriority: -1,
+		onModifyMove: function (move, pokemon) {
+			if (move.type === 'Normal' && move.id !== 'naturalgift' && !move.isZ) {
+				move.type = 'Water';
+				if (move.category !== 'Status') pokemon.addVolatile('liquidvoice');
+			}
+		},
+		effect: {
+			duration: 1,
+			onBasePowerPriority: 8,
+			onBasePower: function (basePower, pokemon, target, move) {
+				return this.chainModify([0x1333, 0x1000]);
+			},
+		},
+		
 };
