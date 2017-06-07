57919e6b8f1ba5a2cc61a6f388909b4f50a9e45b
VXN <ChoiceBandLunala@users.noreply.github.com>
2017-06-06 23:03:13 -0400

    Update pokedex.js

--- mods/vu/pokedex.js
+++ mods/vu/pokedex.js
@@ -1,18 +1,9 @@
 'use strict';
 
 exports.BattlePokedex = {
-unown: {
-		num: 201,
-		species: "Unown",
-		baseForme: "A",
-		types: ["Psychic"],
-		gender: "N",
+	unown: {
+		inherit: true,
 		baseStats: {hp: 75, atk: 75, def: 75, spa: 75, spd: 75, spe: 75},
 		abilities: {0: "Protean"},
-		heightm: 0.5,
-		weightkg: 5,
-		color: "Black",
-		eggGroups: ["Undiscovered"],
-		otherForms: ["unownb", "unownc", "unownd", "unowne", "unownf", "unowng", "unownh", "unowni", "unownj", "unownk", "unownl", "unownm", "unownn", "unowno", "unownp", "unownq", "unownr", "unowns", "unownt", "unownu", "unownv", "unownw", "unownx", "unowny", "unownz", "unownexclamation", "unownquestion"],
 	},
 };
