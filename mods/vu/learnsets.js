2ae1ebd24c655f8691019c00bf6a1fa180c652ef
VXN <ChoiceBandLunala@users.noreply.github.com>
2017-06-07 08:42:57 -0400

    Update learnsets.js

--- mods/vu/learnsets.js
+++ mods/vu/learnsets.js
@@ -4,4 +4,9 @@ exports.BattleLearnsets = {
 	unown: {learnset: {
 		sketch: ["7M", "7L1", "6L1", "5L1", "4L1", "3L1"],
 	}},
+	pyukumuku: {learnset: {
+		inherit: true,
+		scald: ["7M"],
+		icywind: ["7M"],
+	}},
 };
