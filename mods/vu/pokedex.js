b45b379f9bfaa060d2ed7552ddf709fa1e20d797
VXN <ChoiceBandLunala@users.noreply.github.com>
2017-06-06 21:05:14 -0400

    Add unown

--- mods/vu/pokedex.js
+++ mods/vu/pokedex.js
@@ -1,5 +1,18 @@
 'use strict';
 
 exports.BattlePokedex = {
-
+unown: {
+		num: 201,
+		species: "Unown",
+		baseForme: "A",
+		types: ["Psychic"],
+		gender: "N",
+		baseStats: {hp: 75, atk: 75, def: 75, spa: 75, spd: 75, spe: 75},
+		abilities: {0: "Protean"},
+		heightm: 0.5,
+		weightkg: 5,
+		color: "Black",
+		eggGroups: ["Undiscovered"],
+		otherForms: ["unownb", "unownc", "unownd", "unowne", "unownf", "unowng", "unownh", "unowni", "unownj", "unownk", "unownl", "unownm", "unownn", "unowno", "unownp", "unownq", "unownr", "unowns", "unownt", "unownu", "unownv", "unownw", "unownx", "unowny", "unownz", "unownexclamation", "unownquestion"],
+	},
 };
