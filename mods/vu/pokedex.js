694c00ceffc1c20d295e992227188351ed4f520f
VXN <ChoiceBandLunala@users.noreply.github.com>
2017-06-07 08:39:06 -0400

    Add pyukumuku

--- mods/vu/pokedex.js
+++ mods/vu/pokedex.js
@@ -6,4 +6,9 @@ exports.BattlePokedex = {
 		baseStats: {hp: 75, atk: 75, def: 75, spa: 75, spd: 75, spe: 75},
 		abilities: {0: "Protean"},
 	},
+	pyukumuku: {
+		inherit: true,
+		baseStats: {hp: 60, atk: 30, def: 130, spa: 75, spd: 130, spe: 5},
+		abilities: {0: "Prankster", 1: "Oblivious", H: "Unaware"},
+	},
 };
