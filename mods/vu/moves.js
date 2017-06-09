a4d5bf8cdce2f7742b94ddcee379234bcce34032
VXN <Garbodor@users.noreply.github.com>
2017-06-09 17:58:22 -0400

    Create moves.js

--- mods/vu/moves.js
+++ mods/vu/moves.js
@@ -22,22 +22,6 @@ exports.BattleMovedex = {
 		type: "Fairy",
 		contestType: "Clever",
 	},
-	"hiddenpowernormal": {
-		accuracy: 100,
-		basePower: 60,
-		category: "Special",
-		desc: "",
-		shortDesc: "",
-		id: "hiddenpower",
-		name: "Hidden Power Normal",
-		pp: 15,
-		priority: 0,
-		flags: {protect: 1, mirror: 1},
-		secondary: false,
-		target: "normal",
-		type: "Normal",
-		contestType: "Clever",
-	},
 	"hiddenpower": {
 		inherit: true,
 		onModifyMove: function (move, pokemon) {
