44ee9fa71aace6643502e3fbdb208cb0ab3014c7
VXN <Garbodor@users.noreply.github.com>
2017-06-09 18:37:15 -0400

    Create formats-data.js

--- mods/custommons/formats-data.js
+++ mods/custommons/formats-data.js
@@ -1,5 +1,16 @@
 'use strict';
 
 exports.BattleFormatsData = {
-
+	pyrix: {
+		tier: "Custom",
+	},
+	malwarriormage: {
+		tier: "Custom",
+	},
+	malwarriorfighter: {
+		tier: "Custom",
+	},
+	naucess: {
+		tier: "Custom",
+	},
 };
