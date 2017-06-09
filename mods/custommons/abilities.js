f9e7caf6ff536883532dd1f9e45309d81d845a4f
VXN <Garbodor@users.noreply.github.com>
2017-06-09 18:23:35 -0400

    Create abilities.js

--- mods/custommons/abilities.js
+++ mods/custommons/abilities.js
@@ -1,6 +1,17 @@
 'use strict';
 
 exports.BattleAbilities = {
+	"magicalpower": {
+		shortDesc: "This Pokemon's Special Attack is doubled.",
+		onModifySpAPriority: 5,
+		onModifySpA: function (spa) {
+			return this.chainModify(2);
+		},
+		id: "magicalpower",
+		name: "Magical Power",
+		rating: 5,
+		num: -100,
+	},
 	"firestarter": {
 		shortDesc: "This Pokemon's Fire-type attacks have their power multiplied by 1.5.",
 		onBasePowerPriority: 8,
