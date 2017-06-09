1d52c4892df78fd6b7160d72fa3c4a138f7042c5
VXN <Garbodor@users.noreply.github.com>
2017-06-09 18:12:09 -0400

    Create abilities.js

--- mods/custommons/abilities.js
+++ mods/custommons/abilities.js
@@ -1,26 +1,6 @@
 'use strict';
 
 exports.BattleAbilities = {
-	"modifyprogram": {
-		onBeforeMovePriority: 0.5,
-		onBeforeMove: function (attacker, defender, move) {
-			if (attacker.template.baseSpecies !== 'Malwarrior' || attacker.transformed) return;
-			if (move.category === 'Special') {
-				attacker.formeChange('Malwarrior-Mage');
-				this.add('-formechange', attacker, 'Malwarrior-Mage', '[from] ability: Modify Program');
-			} else if (move.category === 'Psysical') {
-				attacker.formeChange('Malwarrior-Fighter');
-				this.add('-formechange', attacker, 'Name-Fighter', '[from] ability: Modify Program');
-			} else if (move.category === 'Status') {
-				attacker.formeChange('Malwarrior');
-				this.add('-formechange', attacker, 'Malwarrior', '[from] ability: Modify Program');
-			},
-		},
-		id: "modifyprogram",
-		name: "Modify Program",
-		rating: 5,
-		num: -100,
-	},
 	"firestarter": {
 		shortDesc: "This Pokemon's Fire-type attacks have their power multiplied by 1.5.",
 		onBasePowerPriority: 8,
