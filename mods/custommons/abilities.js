f04abba9a68fa76b578da614e0bf815b1de498b2
VXN <BowserMemoryML@users.noreply.github.com>
2017-06-09 11:59:07 -0400

    Create abilities.js

--- mods/custommons/abilities.js
+++ mods/custommons/abilities.js
@@ -19,6 +19,20 @@ exports.BattleAbilities = {
 		id: "modifyprogram",
 		name: "Modify Program",
 		rating: 5,
-		num: -1,
+		num: -100,
+	},
+	"firestarter": {
+		shortDesc: "This Pokemon's Fire-type attacks have their power multiplied by 1.5.",
+		onBasePowerPriority: 8,
+		onBasePower: function (basePower, attacker, defender, move) {
+			if (move.type === 'Fire') {
+				this.debug('Firestarter boost');
+				return this.chainModify(1.5);
+			}
+		},
+		id: "firestarter",
+		name: "Firestarter",
+		rating: 4,
+		num: -101,
 	},
 };
