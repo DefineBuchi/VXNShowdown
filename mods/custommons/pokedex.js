a64f0e90fd27988183841ab13bae782dbadd1f2f
VXN <BowserMemoryML@users.noreply.github.com>
2017-06-09 08:58:31 -0400

    Add our first pokem

--- mods/custommons/pokedex.js
+++ mods/custommons/pokedex.js
@@ -1,5 +1,15 @@
 'use strict';
 
 exports.BattlePokedex = {
-
+	pyrix: {
+		num: -9000,
+		species: "Pyrix",
+		types: ["Fire", "Fairy"],
+		baseStats: {hp: 110, atk: 60, def: 85, spa: 120, spd: 90, spe: 70},
+		abilities: {0: "Intimidate", 1: "Storm Drain", H: "Simple"},
+		heightm: 0.7,
+		weightkg: 22.2,
+		color: "Red",
+		eggGroups: ["Amorphous"],
+	},
 };
