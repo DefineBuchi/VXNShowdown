c5f1025269abc5c0254b1c1cf9cca195eb5eee26
Garbodor <Garbodor@users.noreply.github.com>
2017-06-12 08:53:12 -0400

    Create moves.js

---
+++ mods/custommons/moves.js
@@ -0,0 +1,28 @@
+'use strict';
+
+exports.BattleMovedex = {
+	"magicalpunch": {
+		num: -9000,
+		accuracy: 100,
+		basePower: 20,
+		category: "Physical",
+		desc: "Hits two to five times. Has a 1/3 chance to hit two or three times, and a 1/6 chance to hit four or five times. If one of the hits breaks the target's substitute, it will take damage for the remaining hits. If the user has the Ability Skill Link, this move will always hit five times.",
+		shortDesc: "Hits 2-5 times in one turn.",
+		id: "magicalpunch",
+		name: "Magical Punch",
+		pp: 15,
+		priority: 0,
+		flags: {
+			contact: 1,
+			protect: 1,
+			mirror: 1,
+			punch: 1
+		},
+		multihit: [2, 5],
+		secondary: false,
+		target: "normal",
+		type: "Normal",
+		zMovePower: 100,
+		contestType: "Cute",
+	},
+};
